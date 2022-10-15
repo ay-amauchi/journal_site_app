e53241f9603b948b07ac14b47893b997e3a710d1
ay-amauchi <shanneicai0124@gmail.com>
2022-10-15 13:55:16 +0900

    課題1完了

---
+++ resources/views/articles/show.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>article show</title>
+</head>
+<body>
+    <h1>論文詳細</h1>
+    <p>タイトル：{{ $articles->title }}</p>
+    <p>{!! nl2br(e($articles->body)) !!}</p>
+</body>
+</html>
